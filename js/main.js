7bb94900d1d90a7f2b7ce4a4d44f5340e2c10db8
andrewpham74@gmail.com <andrewpham74@gmail.com>
2018-04-11 13:46:27 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
-console.log("up and running!");
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+// var cardTwo = cards[1];
+var cardThree = cards[2];
+// var cardFour = cards[3];
+
+cardsInPlay.push(cardOne); //?
+// cardsInPlay.push(cardTwo); //?
+cardsInPlay.push(cardThree); //?
+// cardsInPlay.push(cardFour); //?
+
+console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
+// console.log("User flipped " + cardFour);
+
+if (cardsInPlay.length === 2){
+
+	if (cardsInPlay[0] === cardsInPlay[2]) { 
+		alert("You found a match!");
+	} 
+	else {
+        alert("Sorry, try again.");
+	}
 
 
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
-
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
-
+}
