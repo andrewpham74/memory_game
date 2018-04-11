98b62e9a7300301568276d2ae824edc89fd37ed4
andrewpham74@gmail.com <andrewpham74@gmail.com>
2018-04-11 15:47:14 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -3,7 +3,35 @@
 // var cardThree = "king";
 // var cardFour = "king";
 
-var cards = ["queen", "queen", "king", "king"];
+// var cards = ["queen", "queen", "king", "king"];
+
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage:"images/queen-of-hearts.png"
+},
+
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+},
+
+];
+
 
 var cardsInPlay = [];
 
@@ -33,9 +61,14 @@ function checkForMatch(){
 
 function flipCard(cardId) {
 
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId])
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank)
 	checkForMatch();
+
+	
+
 }
 
 flipCard(0);
