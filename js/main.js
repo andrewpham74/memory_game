308bf2ae6c4c2ca72f4a10e1cc37bcabc016acd0
andrewpham74@gmail.com <andrewpham74@gmail.com>
2018-04-11 15:11:34 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -7,29 +7,47 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-// var cardTwo = cards[1];
-var cardThree = cards[2];
-// var cardFour = cards[3];
+function checkForMatch(){
+	
+	if (cardsInPlay.length === 2){
 
-cardsInPlay.push(cardOne); //?
-// cardsInPlay.push(cardTwo); //?
-cardsInPlay.push(cardThree); //?
-// cardsInPlay.push(cardFour); //?
+			if (cardsInPlay[0] === cardsInPlay[1]) { 
+				alert("You found a match!");
+			} 
+			else {
+		        alert("Sorry, try again.");
+			}
 
-console.log("User flipped " + cardOne);
-// console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
-// console.log("User flipped " + cardFour);
-
-if (cardsInPlay.length === 2){
-
-	if (cardsInPlay[0] === cardsInPlay[2]) { 
-		alert("You found a match!");
-	} 
-	else {
-        alert("Sorry, try again.");
 	}
 
-
 }
+// var cardOne = cards[0];
+// var cardTwo = cards[1];
+// var cardThree = cards[2];
+// var cardFour = cards[3];
+
+// cardsInPlay.push(cardOne); //?
+// cardsInPlay.push(cardTwo); //?
+// cardsInPlay.push(cardThree); //?
+// cardsInPlay.push(cardFour); //?
+
+function flipCard(cardId) {
+
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId])
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardTwo);
+// console.log("User flipped " + cardThree);
+// console.log("User flipped " + cardFour);
+
+
+
+
